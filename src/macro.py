80ca122410a8bd9d68b7b65fa076b955bc8e7646
Stepan <stepa15.b@gmail.com>
2025-04-03 15:03:04 +0200

    minor changes

--- src/macro.py
+++ src/macro.py
@@ -11,17 +11,27 @@ fake_mouse = mouse.Controller()
 
 # Handle recording places clicked
 def on_click(x, y, button, pressed):
+    """Handles mouse clicks while recording macros
+
+    Args:
+        x (int): x-axis coordinate
+        y (int): y-axis coordinate
+        button (string): Mouse button pressed
+        pressed (boolean): If button was pressed
+    """
     if pressed:
         action = "pressed"
         macro_positions.append([x, y, int(time.time())])
         print(f"{button} {action} at {x},{y}")
 
 def exit_script():
+    """Exits the script"""
     print("Script closing...")
     exit()
 
 # Start recording macros
 def start_record_macros():
+    """Starts recording macros"""
     global macros_running
     macros_running = False
     global mouse_listener
@@ -32,6 +42,7 @@ def start_record_macros():
 
 # Stop recording macros
 def stop_record_macros():
+    """Stops recording macros"""
     global macros_running
     macros_running = False
     global mouse_listener
@@ -41,6 +52,7 @@ def stop_record_macros():
         
 # Start the loop for running macros
 def start_macro():
+    """Start the macro loop"""
     if len(macro_positions) < 1:
         return
     global macros_running
@@ -50,30 +62,33 @@ def start_macro():
     
 # Loop for running the macros
 def macro_loop():
-        global macros_running
-        while macros_running:
-            
-            for i in range(len(macro_positions)):
-                if not macros_running: 
-                    break
+    """Macro loop"""
+    global macros_running
+    while macros_running:
+        
+        for i in range(len(macro_positions)):
+            if not macros_running: 
+                break
 
-                if i == 0:
-                    fake_mouse.position = (macro_positions[i][0], macro_positions[i][1])
-                    time.sleep(0.1)
-                    fake_mouse.click(mouse.Button.left)
-                else:
-                    wait_time = macro_positions[i][2] - macro_positions[i-1][2]
-                    time.sleep(max(0.1, wait_time))
-                    fake_mouse.position = (macro_positions[i][0], macro_positions[i][1])
-                    fake_mouse.click(mouse.Button.left)
+            if i == 0:
+                fake_mouse.position = (macro_positions[i][0], macro_positions[i][1])
+                time.sleep(0.1)
+                fake_mouse.click(mouse.Button.left)
+            else:
+                wait_time = macro_positions[i][2] - macro_positions[i-1][2]
+                time.sleep(max(0.1, wait_time))
+                fake_mouse.position = (macro_positions[i][0], macro_positions[i][1])
+                fake_mouse.click(mouse.Button.left)
                     
 def end_macro():
+    """Ends the macro loop"""
     global macros_running
     macros_running = False
     print("Macro stopped")
 
 # Start the hotkey listener
 def Macros():
+    # Set your custom hotkeys here
     hotkeys = {
         '<ctrl>+<alt>+h': start_record_macros,  # Start recording
         '<ctrl>+<alt>+q': stop_record_macros,   # Stop recording
